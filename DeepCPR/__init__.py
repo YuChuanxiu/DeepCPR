867c3c02c458320f419c48b44b2d75b28cfae25b
yuchuanxiu <222301019@csu.edu.cn>
2024-02-02 10:53:14 +0800

    Create __init__.py

---
+++ DeepCPR/__init__.py
@@ -0,0 +1,5 @@
+from .DeepCPR import data_resolution
+from .csv_merge import peaktable
+from .OPLSDA import OPLS, scatter_cluster, vip_objection, heatmap, permutation_test
+from .NetCDF import netcdf_reader
+from .DeepCS import Chromseg
