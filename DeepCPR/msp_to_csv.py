7c0757cf299e01adca23ffe4b0761becd5aa59c2
yuchuanxiu <222301019@csu.edu.cn>
2024-04-02 09:36:37 +0800

    Update msp_to_csv.py

--- DeepCPR/msp_to_csv.py
+++ DeepCPR/msp_to_csv.py
@@ -5,22 +5,27 @@ Created on Thu Feb  1 19:15:36 2024
 @author: ZNDX001
 """
 import pandas as pd
+import os
 
 
 def msp_to_csv(msp_file, csv_save_path, csv_save_name):
     with open(msp_file, 'r') as file:
         lines = file.readlines()
 
-    # 解析数据
+    # loading data
     peaks = []
     for line in lines:
-        if line.startswith('Name') or line.startswith('Num Peaks'):
+        if line.startswith('Name') or line.startswith('RT') or line.startswith('Num Peaks'):
+            continue
+        if line.startswith('InChIKey') or line.startswith('Synon') or line.startswith('Retention_index') or line.startswith('Formula') or line.startswith('MW'):
+            continue
+        if line.startswith('ExactMass') or line.startswith('CAS#') or line.startswith('DB#') or line.startswith('Comments'):
             continue
         parts = line.split()
         if len(parts) == 2:
             m_z, intensity = parts
             peaks.append({'m/z': float(m_z), 'intensity': float(intensity)})
 
-    # 转换为 DataFrame
+    # transform to DataFrame
     df = pd.DataFrame(peaks)
     df.to_csv(csv_save_path + '/' + csv_save_name, index=False, header=False)
